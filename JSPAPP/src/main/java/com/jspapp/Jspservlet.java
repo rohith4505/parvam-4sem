2508cb2e4d7831827be3867258ac17069fbf3dc1
rohith4505 <rohithmohanrohithmohan@gmail.com>
2026-05-19 15:47:06 +0530

    updaed code

---
+++ JSPAPP/src/main/java/com/jspapp/Jspservlet.java
@@ -0,0 +1,26 @@
+package com.jspapp;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Jspservlet
+ */
+@WebServlet("/Jspservlet")
+public class Jspservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd=request.getRequestDispatcher("GREET.jsp");
+		rd.forward(request, response);
+	}
+
+}
