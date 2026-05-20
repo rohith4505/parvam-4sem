e5c30164f21ab8a4dee1f573ba254bcde10ea2d8
rohith4505 <rohithmohanrohithmohan@gmail.com>
2026-05-20 16:03:53 +0530

    updaed code

---
+++ firstmavenproject/src/main/java/com/student/Student1.java
@@ -0,0 +1,24 @@
+package com.student;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Student1
+ */
+@WebServlet("/Student1")
+public class Student1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("student.html");
+	}
+
+}
